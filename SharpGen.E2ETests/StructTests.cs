c91c001da893f21bf98292eeeaca973baefb61d4
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2017-06-03 16:29:16 -0700

    Added test for testing member order for a struct with multiple members.

--- SharpGen.E2ETests/StructTests.cs
+++ SharpGen.E2ETests/StructTests.cs
@@ -47,5 +47,43 @@ namespace SharpGen.E2ETests
             var field = (IFieldSymbol)fieldMember;
             Assert.Equal(compilation.GetTypeByMetadataName("System.Int32"), field.Type);
         }
+
+        [Fact]
+        public void StructWithMultipleMembersGeneratesStructWithMembersInCorrectOrder()
+        {
+            var testDirectory = GenerateTestDirectory();
+            var config = new Config.ConfigFile
+            {
+                Namespace = nameof(StructWithMultipleMembersGeneratesStructWithMembersInCorrectOrder),
+                Assembly = nameof(StructWithMultipleMembersGeneratesStructWithMembersInCorrectOrder),
+                IncludeDirs = { GetTestFileIncludeRule() },
+                Includes =
+                {
+                    CreateCppFile(testDirectory, "structWithMultipleMembers", @"
+                        struct Test {
+                            int field1;
+                            int field2;
+                        };
+                    ")
+                },
+                Bindings =
+                {
+                    new Config.BindRule("int", "System.Int32")
+                }
+            };
+
+            var result = RunWithConfig(testDirectory, config);
+            AssertRanSuccessfully(result.exitCode, result.output);
+
+            var compilation = GetCompilationForGeneratedCode(testDirectory);
+            var structType = compilation.GetTypeByMetadataName($"{nameof(StructWithMultipleMembersGeneratesStructWithMembersInCorrectOrder)}.Test");
+            var attributes = structType.GetAttributes();
+            var structLayoutAttribute = attributes.FirstOrDefault(attr => attr.AttributeClass.Name == nameof(StructLayoutAttribute));
+            Assert.NotNull(structLayoutAttribute);
+            Assert.Equal((int)LayoutKind.Sequential, structLayoutAttribute.ConstructorArguments.First().Value);
+            var members = structType.GetMembers().ToArray();
+            Assert.Equal("Field1", members[0].Name);
+            Assert.Equal("Field2", members[1].Name);
+        }
     }
 }
