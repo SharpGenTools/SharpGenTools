103c2bb2179fc0e0a6d04071ef62a0a0332377b9
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2017-09-04 22:00:38 -0500

    Use Appveyor's deploy tooling instead of my own script

--- deploy.ps1
+++
@@ -1,8 +0,0 @@
-if(!($env:APPVEYOR_PULL_REQUEST_NUMBER)) {
-    foreach ($artifactName in $artifacts.keys) {
-        dotnet nuget push $artifacts[$artifactName].path -k $env:MYGET_API_KEY -s https://www.myget.org/F/sharpgentools/api/v2/package 
-        if($env:APPVEYOR_REPO_TAG) {
-            dotnet nuget push $artifacts[$artifactName].path -k $env:NUGET_API_KEY -s https://www.nuget.org/api/v2/package
-        }
-    }
-}
