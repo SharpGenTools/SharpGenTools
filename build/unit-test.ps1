a90bbad51a1db518a3c84f079226757bffe96486
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2018-03-12 18:23:00 -0500

    Add missing $

--- build/unit-test.ps1
+++ build/unit-test.ps1
@@ -29,7 +29,7 @@ function Test
         return ./build/Run-OpenCover -executable $dotnetExe -arguments $arguments -filters $filters 
     }
     else {
-        dotnet test "$project/project.csproj" --no-build --no-restore | Write-Host
+        dotnet test "$project/$project.csproj" --no-build --no-restore | Write-Host
         return $LastErrorCode -eq 0
     }
 
