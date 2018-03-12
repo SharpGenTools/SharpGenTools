0442ac6bc8da0f8c7cfc60e4a9193728d7c25174
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2018-03-12 17:39:37 -0500

    Make sure we build the tests in the build script.

--- build.ps1
+++ build.ps1
@@ -2,6 +2,12 @@ Param(
     [string] $Configuration = "Debug"
 )
 
+dotnet build -c $Configuration
+
+if ($LastExitCode -ne 0) {
+    exit 1
+}
+
 dotnet pack -c $Configuration
 
 if ($LastExitCode -ne 0) {
