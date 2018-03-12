812a9d59bf4dd1136fcd72502ccfab1780f2727f
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2018-03-12 17:51:44 -0500

    Support only one MSBuild instance found in path.

--- build/build-outerloop.ps1
+++ build/build-outerloop.ps1
@@ -6,13 +6,19 @@ Param(
 $env:Path += ";C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\"
 
 $msbuildLocations = & where.exe msbuild
+$msbuildExe = $null
 
 if ($msbuildLocations -eq $null) {
     Write-Error "Unable to locate MSBuild"
     return $false
 }
+elseif ($msbuildLocations -is [array]) {
+    $msbuildExe = $msbuildLocations[0]
+}
+else {
+    $msbuildExe = $msbuildLocations
+}
 
-$msbuildExe = $msbuildLocations[0]
 $msbuildParameters = "./SdkTests/SdkTests.sln", "/restore", "/m", "/v:n"
 $coverageFilter = @("+[SharpGen]*")
 
