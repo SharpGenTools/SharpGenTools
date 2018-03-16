94eed0a25b3808c78ca0d19484fa1513bcde3c38
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2018-03-16 13:14:07 -0500

    Add some simple outerloop codegen tests for structs with native marshalling.

--- SdkTests/Native/Struct/StructNative.h
+++ SdkTests/Native/Struct/StructNative.h
@@ -1,19 +1,42 @@
 #define STRUCTLIB_API __declspec(dllexport)
 #define STRUCTLIB_FUNC(RET) extern "C" __declspec(dllexport) RET __stdcall
 
-STRUCTLIB_API struct SimpleStruct
+struct SimpleStruct
 {
 	int i;
 	int j;
 };
 
-STRUCTLIB_API struct IntToBoolArray
+struct StructWithArray
 {
 	int i[3];
+	double j;
+};
+
+union TestUnion
+{
+	int integer;
+	float decimal;
+};
+
+struct BitField
+{
+	int firstBit : 1;
+	int lastBits : 31;
+};
+
+struct AsciiTest
+{
+	char SmallString[10];
+	char* LargeString;
+};
+
+struct Utf16Test
+{
+	wchar_t SmallString[10];
+	wchar_t* LargeString;
 };
 
 STRUCTLIB_FUNC(SimpleStruct) GetSimpleStruct();
 
-STRUCTLIB_FUNC(IntToBoolArray) GetIntToBoolArray();
-
-STRUCTLIB_FUNC(bool) And(IntToBoolArray array);
+STRUCTLIB_FUNC(void) ForceMarshalTo(StructWithArray, TestUnion, BitField, AsciiTest, Utf16Test);
