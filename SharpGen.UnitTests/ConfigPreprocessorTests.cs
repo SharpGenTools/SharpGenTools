24e852c7aa8c0db5795bd15daff8b7e6eca60e8b
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2018-04-06 14:53:20 -0500

    Added tests for the config file preprocessor.

---
+++ SharpGen.UnitTests/ConfigPreprocessorTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Linq;
+using SharpGen.Config;
+using Xunit;
+
+namespace SharpGen.UnitTests
+{
+    public class ConfigPreprocessorTests
+    {
+        [Fact]
+        public void IfDefIncludesChildrenWhenMacroDefined()
+        {
+            using (var stringReader = new StringReader(
+                Preprocessor.Preprocess(
+                    $"<root xmlns:pre='{ConfigFile.NS}'><pre:ifdef name='Defined'><child /></pre:ifdef></root>",
+                    "Defined")))
+            {
+                var document = XDocument.Load(stringReader);
+                Assert.Single(document.Descendants("child"));
+            }
+        }
+
+        [Fact]
+        public void IfDefExcludesChildrenWhenMacroUndefined()
+        {
+            using (var stringReader = new StringReader(
+                Preprocessor.Preprocess(
+                    $"<root xmlns:pre='{ConfigFile.NS}'><pre:ifdef name='Undefined'><child /></pre:ifdef><child /></root>")))
+            {
+                var document = XDocument.Load(stringReader);
+                Assert.Single(document.Descendants("child"));
+            }
+        }
+
+        [Fact]
+        public void IfDefSupportsOrOperator()
+        {
+            using (var stringReader = new StringReader(
+                Preprocessor.Preprocess(
+                    $"<root xmlns:pre='{ConfigFile.NS}'><pre:ifdef name='Undefined|Defined'><child /></pre:ifdef></root>",
+                    "Defined")))
+            {
+                var document = XDocument.Load(stringReader);
+                Assert.Single(document.Descendants("child"));
+            }
+        }
+
+        [Fact]
+        public void IfNDefExcludesChildrenWhenMacroDefined()
+        {
+            using (var stringReader = new StringReader(
+                Preprocessor.Preprocess(
+                    $"<root xmlns:pre='{ConfigFile.NS}'><pre:ifndef name='Defined'><child /></pre:ifndef></root>",
+                    "Defined")))
+            {
+                var document = XDocument.Load(stringReader);
+                Assert.Empty(document.Descendants("child"));
+            }
+        }
+
+        [Fact]
+        public void IfNDefIncludesChildrenWhenMacroUndefined()
+        {
+            using (var stringReader = new StringReader(
+                Preprocessor.Preprocess(
+                    $"<root xmlns:pre='{ConfigFile.NS}'><pre:ifndef name='Undefined'><child /></pre:ifndef></root>"
+                    )))
+            {
+                var document = XDocument.Load(stringReader);
+                Assert.Single(document.Descendants("child"));
+            }
+        }
+    }
+}
