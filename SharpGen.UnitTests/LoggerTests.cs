a7f1c7deeb1af632948ac1453a700445cb096cf3
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2018-04-09 21:39:22 -0500

    Add unit test for Logger.Fatal

---
+++ SharpGen.UnitTests/LoggerTests.cs
@@ -0,0 +1,23 @@
+using Xunit;
+using FakeItEasy;
+using SharpGen.Logging;
+using System;
+
+namespace SharpGen.UnitTests
+{
+    public class LoggerTests
+    {
+        [Fact]
+        public void LoggerFatalCallsLoggerOutputExit()
+        {
+            var output = A.Fake<ILogger>();
+
+            var logger = new Logger(output);
+
+            logger.Fatal("Fatal error");
+
+            A.CallTo(() => output.Log(A<LogLevel>.Ignored, A<LogLocation>.Ignored, A<string>.Ignored, A<string>.Ignored, A<string>.Ignored, A<Exception>.Ignored, A<object[]>.Ignored)).MustHaveHappened();
+            A.CallTo(() => output.Exit(A<string>.Ignored, A<int>.Ignored)).MustHaveHappened();
+        }
+    }
+}
