49f6153863fd583088b65ccea88bc590ce047ab2
Andrew Boyarshin <andrew.boyarshin@gmail.com>
2020-04-24 23:41:56 +0700

    PlatformDetection now has static readonly fields

--- SharpGen.Runtime/PlatformDetection.cs
+++ SharpGen.Runtime/PlatformDetection.cs
@@ -4,9 +4,9 @@ namespace SharpGen.Runtime
 {
     public static class PlatformDetection
     {
-        public static bool IsWindows => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+        public static readonly bool IsWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
 
-        public static bool IsItaniumSystemV => RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ||
-                                               RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+        public static readonly bool IsItaniumSystemV = RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ||
+                                                       RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
     }
 }
