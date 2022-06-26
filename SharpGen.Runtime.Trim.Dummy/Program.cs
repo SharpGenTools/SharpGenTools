b651428a473cb8bf741172856780770c4603b64d
Sewer 56 <seweratwork@gmail.com>
2022-06-26 18:42:11 +0100

    Added: COM Project to Dummy

--- SharpGen.Runtime.Trim.Dummy/Program.cs
+++ SharpGen.Runtime.Trim.Dummy/Program.cs
@@ -4,3 +4,7 @@
 // test my trimming with `dotnet publish -r win-x64`
 
 Console.WriteLine("Hello SharpGenTools");
+
+// Please note that you'll need to `dotnet pack` SharpGenTools.Sdk and
+// put it in the `SharpGen.Runtime.COM/LocalPackages` package for SharpGen.Runtime.COM
+// dependency to compile.
